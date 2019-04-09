0f2141fc4f56fbc4c6d7543b6cd8c88e291282d2
sodeeq olapade <olapadeabiodun21@email.com>
2019-04-09 20:39:34 +0100

    Implement the following:
    1) Difference function and associated tests
    2) IndefiniteMultiplication and associated tests
    3) Division and associated test

--- libs.js
+++ libs.js
@@ -1,8 +1,13 @@
 module.exports.sum = function(a, b) {
-  if (a === NaN || b === NaN) {
-    throw new Error("Params must be a number");
+  if (arguments.length === 0) {
+    throw new Error("Parameters cannot be empty");
   }
-  if (typeof a !== "number" || typeof b !== "number") {
+  if (
+    Number.isNaN(a) ||
+    Number.isNaN(b) ||
+    typeof a !== "number" ||
+    typeof b !== "number"
+  ) {
     throw new Error("Params must be a number");
   }
   return a + b;
@@ -19,4 +24,47 @@ module.exports.indefiniteSum = function(...params) {
   });
 
   return params.reduce((acc, current) => acc + current, 0);
+};
+
+module.exports.difference = function(a, b) {
+  if (
+    Number.isNaN(a) ||
+    Number.isNaN(b) ||
+    typeof a !== "number" ||
+    typeof b !== "number"
+  ) {
+    throw new Error("Params must be a number");
+  }
+  return a - b;
+};
+
+module.exports.indefiniteMultiplication = function(...params) {
+  if (params.length === 0) {
+    throw new Error("Enter at least a parameter");
+  }
+  params.forEach(a => {
+    if (Number.isNaN(a) || typeof a !== "number") {
+      throw new Error("All Params must be a number");
+    }
+  });
+  return params.reduce((acc, current) => {
+    return acc * current;
+  }, 1);
+};
+
+module.exports.division = function(a, b) {
+  if (arguments.length !== 2) {
+    throw new Error("You are expected to enter two parameters");
+  }
+
+  if (
+    Number.isNaN(a) ||
+    Number.isNaN(b) ||
+    typeof a !== "number" ||
+    typeof b !== "number"
+  ) {
+    throw new Error("Params must be a number");
+  }
+
+  return a / b;
 };
--- tests/unit/libs.test.js
+++ tests/unit/libs.test.js
@@ -1,20 +1,20 @@
 const lib = require("../../libs");
 
 describe("sum", () => {
+  it("throws an error if no argument is provided", () => {
+    expect(() => {
+      lib.sum();
+    }).toThrow();
+  });
+
   it("returns the addition of two positive or negative integers", () => {
     const result = lib.sum(-1, 2);
     expect(result).toBe(1);
   });
 
-  it("throws error if any of the arguments is not a number", () => {
+  it("throws error if any of the arguments is not a number including NaN", () => {
     expect(() => {
-      lib.sum({}, 3);
-    }).toThrow();
-  });
-
-  it("throws error if a or b is NaN", () => {
-    expect(() => {
-      lib.sum(NaN, "b");
+      lib.sum("", 3);
     }).toThrow();
   });
 });
@@ -37,3 +37,50 @@ describe("indefiniteSum", () => {
     }).toThrow();
   });
 });
+
+describe("difference", () => {
+  it("returns the difference between two given integers", () => {
+    const result = lib.difference(3, 7);
+    expect(result).toBe(-4);
+  });
+
+  it("throws error if any of the arguments is not a number including NaN and empty parameter list", () => {
+    expect(() => {
+      lib.difference(NaN, 3);
+    }).toThrow();
+  });
+});
+
+describe("indefiniteMultiplication", () => {
+  it("throws error if no arguments is provided", () => {
+    expect(() => {lib.indefiniteMultiplication()}).toThrow();
+  });
+  it("return multiplication of all arguments", () => {
+    const result = lib.indefiniteMultiplication(1, 2, 3, -1, 4);
+    expect(result).toBe(-24);
+  });
+  it("it should throw an error if any of the argument is not a number including NaN", () => {
+    expect(() => {
+      lib.indefiniteMultiplication(1,{}, 2, 3, NaN, 5);
+    }).toThrow();
+  });
+});
+
+describe("division", () => {
+    it("throws an error if no argument is provided", () => {
+      expect(() => {
+        lib.division(1,2,3);
+      }).toThrow();
+    });
+  
+    it("returns the division of two positive or negative integers", () => {
+      const result = lib.division(-1, 2);
+      expect(result).toBe(-0.5);
+    });
+  
+    it("throws error if any of the arguments is not a number including NaN", () => {
+      expect(() => {
+        lib.division(3, NaN);
+      }).toThrow();
+    });
+  });
